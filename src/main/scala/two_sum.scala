fe223cd4ad504d6fc32b9f0261c178937dc1943e
Emmett Rayes <emmett.rayes@icloud.com>
2024-08-15 23:17:30 +0200

    Add two_sum

---
+++ src/main/scala/two_sum.scala
@@ -0,0 +1,16 @@
+import scala.collection.mutable
+import scala.util.boundary
+import scala.util.boundary.break
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that
+ * they add up to target. You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice. You can return the answer in any order.
+ */
+def two_sum(nums: Array[Int], target: Int): Array[Int] =
+  val complement: mutable.Map[Int, Int] = mutable.HashMap()
+  boundary:
+    for ((n, i) <- nums.zipWithIndex) do
+      if complement.contains(n) then break(Array(i, complement(n))) else
+        complement.put(target - n, i)
+    Array()

e6db95a607f684a633f5fcc9e9c7c068c5540aac
Emmett Rayes <emmett.rayes@icloud.com>
2024-08-15 23:55:45 +0200

    Add group_anagrams

---
+++ src/main/scala/group_anagrams.scala
@@ -0,0 +1,18 @@
+import scala.collection.mutable
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return
+ * the answer in any order. An Anagram is a word or phrase formed by rearranging the letters
+ * of a different word or phrase, typically using all the original letters exactly once.
+ */
+def group_anagrams(strs: Array[String]): List[List[String]] =
+  val frequencies: mutable.Map[Map[Char, Int], mutable.ArrayBuffer[String]] = mutable.Map()
+  for (str <- strs) do
+    val freq: mutable.Map[Char, Int] = mutable.Map().withDefaultValue(0)
+    for (c <- str) do
+      freq.update(c, freq(c) + 1)
+    val m = freq.toMap
+    if frequencies.contains(m)
+    then frequencies(m).append(str)
+    else frequencies.put(m, mutable.ArrayBuffer(str))
+  frequencies.values.collect(array => array.toList).toList

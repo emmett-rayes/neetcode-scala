68add60123ab33a65cd77db012fd2e864eaedb72
Emmett Rayes <emmett.rayes@icloud.com>
2024-08-15 22:34:34 +0200

    Add contains_duplicate

---
+++ src/main/scala/contains_duplicate.scala
@@ -0,0 +1,15 @@
+import scala.collection.mutable
+import scala.util.boundary
+import scala.util.boundary.break
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ */
+def contains_duplicate(nums: Array[Int]): Boolean =
+  val seen: mutable.Set[Int] = mutable.HashSet()
+  boundary:
+    for (n <- nums) do
+      if seen.contains(n) then break(true) else
+        seen.add(n)
+    false

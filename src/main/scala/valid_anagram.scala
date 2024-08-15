f0331fa0b0fd7458743a6a5dea2ae9f9fcc9a2fe
Emmett Rayes <emmett.rayes@icloud.com>
2024-08-15 22:48:33 +0200

    Add valid_anagram

---
+++ src/main/scala/valid_anagram.scala
@@ -0,0 +1,21 @@
+import scala.collection.mutable
+import scala.util.boundary
+import scala.util.boundary.break
+
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ */
+def valid_anagram(s: String, t: String): Boolean =
+  if s.length != t.length then return false
+
+  val frequency: mutable.Map[Char, Int] = mutable.HashMap().withDefaultValue(0)
+  for (c <- s) do
+    frequency.update(c, frequency(c) + 1)
+  for (c <- t) do
+    frequency.update(c, frequency(c) - 1)
+  boundary:
+    for (i <- frequency.values) do
+      if i != 0 then break(false)
+    true
